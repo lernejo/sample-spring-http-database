58e1892e422dd2d7774c1550f842233cac448d22
ledoyen <ledoyen.loic@gmail.com>
2020-01-28 15:50:41 +0100

    Bootstrap with start.spring.io

---
+++ src/main/java/com/github/lernejo/sample/spring/httpdatabase/SampleSpringHttpDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.github.lernejo.sample.spring.httpdatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSpringHttpDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleSpringHttpDatabaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/lernejo/sample/spring/httpdatabase/SampleSpringHttpDatabaseApplicationTests.java
@@ -0,0 +1,12 @@
+package com.github.lernejo.sample.spring.httpdatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleSpringHttpDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
